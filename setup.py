b7468f69c4afea5897db6bdf256828f2bc9fec5d
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2021-05-16 13:54:14 -0700

    Metadata

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='randos',
+    packages=find_packages(where="src"),
+    version='0.0.1',
+    description='A randomness library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_dir={"": "src"},
+    author='Mohammed Aljaroudi',
+    author_email='maljaroudi@sandiego.edu',
+    include_package_data=True,
+    python_requires=">=3.9",
+    install_requires=[],
+    tests_require=['pytest>=3', ],
+    url='https://github.com/aljaroudi/randos-pip'
+)
