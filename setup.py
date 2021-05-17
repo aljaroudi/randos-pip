14338bb5380959d7759188a2ac4c00de8597179f
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2021-05-17 03:50:36 -0700

    v0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='randos',
     packages=find_packages(where="src"),
-    version='0.0.2',
+    version='0.0.3',
     description='A randomness library',
     long_description=long_description,
     long_description_content_type="text/markdown",
