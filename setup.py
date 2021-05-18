97a3ce0161858d47b70dd98d3f9d0a1728d2e440
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2021-05-18 16:24:24 -0700

    v0.0.6 released :sparkles:

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='randos',
     packages=find_packages(where="src"),
-    version='0.0.5',
+    version='0.0.6',
     description='A randomness library',
     long_description=long_description,
     long_description_content_type="text/markdown",
