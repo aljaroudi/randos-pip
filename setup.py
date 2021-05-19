1d133a61e3ef84d5b32b6215d7a07c389410ced3
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2021-05-19 03:10:51 -0700

    0.0.7 :star: updated package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='randos',
-    packages=find_packages(where="src"),
+    packages=find_packages(where="randos"),
     version='0.0.7',
     description='A randomness library',
     long_description=long_description,
