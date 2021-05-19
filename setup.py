937f2b4c225bf6144c6d45effcc13af62cb06c37
Mohammed Aljaroudi <>
2021-05-19 20:58:27 +0000

    0.0.16 :star:

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='randos',
     packages=find_packages(),
-    version='0.0.15',
+    version='0.0.16',
     description='A randomness library',
     long_description=long_description,
     long_description_content_type="text/markdown",
