c9ec54de4aaef040d3a2fb49f478ee17fee73479
Mohammed Aljaroudi <73927882+aljaroudi@users.noreply.github.com>
2021-05-19 03:39:07 -0700

    0.0.10 :star: update files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name='randos',
     packages=find_packages(where="randos"),
-    version='0.0.9',
+    version='0.0.10',
     description='A randomness library',
     long_description=long_description,
     long_description_content_type="text/markdown",
